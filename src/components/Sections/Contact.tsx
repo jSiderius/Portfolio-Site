21d5a8ed41d3e998529ff2e31a9c1ba616fe8159
Joshua <jsiderius0@gmail.com>
2025-09-14 20:19:06 -0400

    Added contact section

--- src/components/Sections/Contact.tsx
+++ src/components/Sections/Contact.tsx
@@ -1,10 +1,55 @@
+import "../../css/Contact.css";
+
 function Contact() {
   return (
     <>
-      <section className="vh-100">
-        <h1>Contact</h1>;
+      <section className="contact-top-container">
+        <h1 className="experience-header">Lets Connect</h1>
+        <UserInputPanel />
+        <div className="contact-socials-container">
+          <div className="d-flex justify-content-flex-start gap-4 fs-1">
+            <img className="icon" src="Icons/Python.svg" alt="" />
+            <img className="icon" src="Icons/React.svg" alt="" />
+            <img className="icon" src="Icons/JavaScript.svg" alt="" />
+            <img className="icon" src="Icons/CSharp.svg" alt="" />
+            <img className="icon" src="Icons/CPlusPlus.svg" alt="" />
+            <img className="icon" src="Icons/Database.svg" alt="" />
+          </div>
+        </div>
       </section>
     </>
+  );
+}
+
+function UserInputPanel() {
+  return (
+    <div className="contact-user-input-panel">
+      <div className="name-email-conatiner">
+        <div className="contact-field-conatiner">
+          <p>Name</p>
+          <input type="text"></input>
+        </div>
+        <div className="contact-field-conatiner">
+          <p>Email</p>
+          <input type="text"></input>
+        </div>
+      </div>
+
+      <div className="contact-field-conatiner">
+        <p>Subject</p>
+        <input type="text"></input>
+      </div>
+
+      <div className="contact-field-conatiner">
+        <p>Message</p>
+        {/* <input type="text" className="message-field"></input> */}
+        <textarea
+          className="message-field"
+          rows={3} // adjust default height
+          placeholder="Type your message..."
+        ></textarea>
+      </div>
+    </div>
   );
 }
 
