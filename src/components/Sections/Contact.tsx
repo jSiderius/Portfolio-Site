af8e17cf86266eb0fd57e163d99a65d8babf3646
Joshua <jsiderius0@gmail.com>
2025-11-18 18:03:43 -0500

    Connected contact form to web3forms

--- src/components/Sections/Contact.tsx
+++ src/components/Sections/Contact.tsx
@@ -1,11 +1,12 @@
+import { useState } from "react";
 import "../../css/Contact.css";
 
 function Contact() {
   return (
     <>
-      <section id="contact-form" className="contact-top-container">
+      <section className="contact-top-container">
         <h1 className="experience-header">Let's Connect</h1>
-        <UserInputPanel />
+        <UserInputForm />
         <div className="contact-socials-container">
           <div className="d-flex justify-content-flex-start gap-4 fs-1">
             <a href="https://github.com/jSiderius">
@@ -24,34 +25,61 @@ function Contact() {
   );
 }
 
-function UserInputPanel() {
+function UserInputForm() {
+  const [result, setResult] = useState("");
+
+  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    formData.append("access_key", "3a99d9ed-4e5e-471e-a899-aae3d65b09c8");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+    setResult(data.success ? "Success!" : "Error");
+  };
+
   return (
-    <div className="contact-user-input-panel border-effect card">
+    <form
+      onSubmit={onSubmit}
+      className="contact-user-input-panel border-effect card"
+    >
       <div className="name-email-conatiner">
         <div className="contact-field-conatiner">
           <p>Name</p>
-          <input type="text"></input>
+          <input className="text-field" type="text" name="name" required />
         </div>
+
         <div className="contact-field-conatiner">
           <p>Email</p>
-          <input type="text"></input>
+          <input className="text-field" type="text" name="email" required />
         </div>
       </div>
 
       <div className="contact-field-conatiner">
         <p>Subject</p>
-        <input type="text"></input>
+        <input className="text-field" type="text" name="subject" />
       </div>
 
       <div className="contact-field-conatiner">
         <p>Message</p>
         <textarea
-          className="message-field"
+          className="message-field text-field"
           rows={3}
           placeholder="Type your message..."
-        ></textarea>
+          name="message"
+          required
+        />
       </div>
-    </div>
+
+      <button className="submission-button" type="submit">
+        Submit
+      </button>
+      <p>{result}</p>
+    </form>
   );
 }
 
